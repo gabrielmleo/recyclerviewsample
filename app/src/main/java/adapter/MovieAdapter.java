23339f7e88f647d90d2268aa92a6730b02f0cb4e
Gabriel <gabrielmoraesleonardo@gmail.com>
2016-10-31 15:01:53 -0200

    Inicio da construcao do adapter do recycler view

---
+++ app/src/main/java/adapter/MovieAdapter.java
@@ -0,0 +1,21 @@
+package adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import model.Movie;
+
+/**
+ * Created by desenvolvedor on 31/10/16.
+ */
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+
+    private ArrayList<Movie> mDataSet;
+
+
+
+
+
+}
--- app/src/main/java/com/example/gabrielmoraes/topmovies/MainActivity.java
+++ app/src/main/java/com/example/gabrielmoraes/topmovies/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
